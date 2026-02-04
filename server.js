74813ae9b22650b7bdb34c534475c877a2c10c7a
yadusc1588@gmail.com <yadusc1588@gmail.com>
2026-02-05 00:22:16 +0530

    completed the server.js file

--- server.js
+++ server.js
@@ -1,33 +1,47 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
 const router = express.Router();
 
+app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send("Hello from the Cars API!");
+let cars = [
+    {id: 1, make: 'Toyota', model: 'Camry', year: 2022, price: 28000},
+    {id: 2, make: 'Tesla', model: 'Model S', year: 2023, price: 25000},
+    {id: 3, make: 'Ford', model: 'F-150', year: 2021, price: 35000}
+]
 
+app.get("/", (req, res) => {
+  res.send("Hello from the Cars API!");
 });
 
-app.get('/api/v1/cars',(req,res)=>{
-res.send("All cars");
-})
+router.get("/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const car = cars.find((car) => car.id ===id);
+    if(!car) return res.status(404).send('Car not found');
+    res.json(car);
+});
 
-app.post('/api/v1/cars',(req,res)=> {
-    res.send('New Car');
-})
+router.get("/", (req, res) => {
+   res.json(cars);
+});
 
-app.put('api/v1/cars/:id',(req,res) => {
-    res.send('Update Car')
-})
+router.post("/", (req, res) => {
+  res.send("Add a New Car");
+});
 
-app.delete('api/v1/cars/:id',(req,res) => {
-    res.send('Delete Car');
-})
-app.get('api/v1/cars/:id',(req,res) => {
-    res.send('Get Car');
-})
+router.put("/:id", (req, res) => {
+  res.send("Update the specific Car");
+});
 
-app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+router.delete("/:id", (req, res) => {
+  res.send("Delete Car");
+});
+
+app.use("/api/v1/cars", router);
+
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`),
+);
