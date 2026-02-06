4fe2b2dfab052bbca0dfc6eaa21a79bca8a16973
yadusc1588@gmail.com <yadusc1588@gmal.com>
2026-02-06 15:45:01 +0530

    Server.js and Package.json files modified

--- server.js
+++ server.js
@@ -5,27 +5,28 @@ const port = 3000;
 
 const router = express.Router();
 
+app.use("/api/v1/cars", router);
 app.use(express.json());
 
 let cars = [
-    {id: 1, make: 'Toyota', model: 'Camry', year: 2022, price: 28000},
-    {id: 2, make: 'Tesla', model: 'Model S', year: 2023, price: 25000},
-    {id: 3, make: 'Ford', model: 'F-150', year: 2021, price: 35000}
-]
+  { id: 1, make: "Toyota", model: "Camry", year: 2022, price: 28000 },
+  { id: 2, make: "Tesla", model: "Model S", year: 2023, price: 25000 },
+  { id: 3, make: "Ford", model: "F-150", year: 2021, price: 35000 },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello from the Cars API!");
 });
 
 router.get("/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const car = cars.find((car) => car.id ===id);
-    if(!car) return res.status(404).send('Car not found');
-    res.json(car);
+  const id = Number(req.params.id);
+  const car = cars.find((car) => car.id === id);
+  if (!car) return res.status(404).send("Car not found");
+  res.json(car);
 });
 
 router.get("/", (req, res) => {
-   res.json(cars);
+  res.json(cars);
 });
 
 router.post("/", (req, res) => {
@@ -40,8 +41,7 @@ router.delete("/:id", (req, res) => {
   res.send("Delete Car");
 });
 
-app.use("/api/v1/cars", router);
-
 app.listen(port, () =>
   console.log(`Server is running on http://localhost:${port}`),
 );
+
